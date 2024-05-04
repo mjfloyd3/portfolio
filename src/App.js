07e0234b58139d9b403eab22329a87e73446ca53
Jack <mjfloyd3@gmail.com>
2024-05-03 21:28:23 -0400

    onemore

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import HomePage from './Pages/HomePage';
 import Sstemplates from './Pages/Sstemplate';
@@ -10,15 +10,15 @@ import Footer from "./Pages/footer";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
-        <Routes>
+      <Router>
+        <Switch>
           <Route index element={<HomePage/>}/>
           <Route path="sstemplates" element={<Sstemplates/>}/>
           <Route path="websiteredesign" element={<WebsiteRedesign/>}/>
           <Route path="officialtemplate" element={<ProductCommunicationTemplate/>}/>
           <Route path="about" element={<About/>}/>
-        </Routes>
-        </HashRouter>
+        </Switch>
+        </Router>
         <Footer/>
     </div>
   );
