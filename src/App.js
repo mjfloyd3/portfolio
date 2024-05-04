dfe10d29289eb41e1973a3542632e11950343cf1
Jack <mjfloyd3@gmail.com>
2024-05-03 22:23:49 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Sstemplates from './Pages/Sstemplate';
 import WebsiteRedesign from './Pages/WebsiteRedesign';
@@ -7,19 +6,10 @@ import ProductCommunicationTemplate from './Pages/CommunicationTemplate';
 import About from "./Pages/About";
 import Footer from "./Pages/footer";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
-  React.useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-}
-
 function App() {
   return (
     <div className="App">
       <Router>
-       <ScrollToTop />
         <Routes>
           <Route index element={<HomePage/>}/>
           <Route path="/sstemplates" element={<Sstemplates/>}/>
