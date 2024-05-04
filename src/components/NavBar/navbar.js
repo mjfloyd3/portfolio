02d5ebc59e12b36815af9b272484526b5d172b0f
Jack <mjfloyd3@gmail.com>
2024-05-04 12:50:45 -0400

    day2

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -8,15 +8,15 @@ const Navbar = () => {
         <nav className="navbar navbar-expand-lg">
           <div className="container">
             <div>
-              <Link to="" className="logo">JF</Link>
+              <Link to="/#" className="logo">JF</Link>
             </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse flex-grow-0" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item pe-4">
-                  <a className="nav-link" href="#selectedwork">Work</a>
+                  <a className="nav-link" href="#/#selectedwork">Work</a>
                 </li>
                 <li className="nav-item pe-4">
                   <Link to="/about" className="nav-link">About</Link>
--- src/components/Work/work.js
+++ src/components/Work/work.js
@@ -13,50 +13,50 @@ const Work = () => {
         <section id="Work">  
             <div className="container">     
                 <div className="workContent">
-                        <div className="row my-5 py-5">
+                        <div className="row my-5 py-md-5 py-0">
                             <h3 id="selectedwork">Selected work</h3>
                             <div className="col">
-                                <a class="text-decoration-none" href="https://xd.adobe.com/view/0edfc61a-4a8c-4575-ab4b-f706505df158-5b8c">
+                                <a className="text-decoration-none" href="https://xd.adobe.com/view/0edfc61a-4a8c-4575-ab4b-f706505df158-5b8c">
                                     <img src={Preview} alt="portal preview" width="90%" />
-                                    <h4 class="mt-3">Research Portal Landing Page &#160; &#160;<i class="bi bi-box-arrow-up-right"></i></h4>
+                                    <h4 className="mt-3">Research Portal Landing Page &#160; &#160;<i class="bi bi-box-arrow-up-right"></i></h4>
                                 </a>
                                 <p class="m-0 small">UI Design | Product Design</p>
                             </div>
                         </div>
-                        <div class="row my-5 py-5">
-                            <div class="col">
-                                <Link to="Sstemplates" class="text-decoration-none">
+                        <div className="row my-5 py-md-5 py-0">
+                            <div className="col">
+                                <Link to="Sstemplates" className="text-decoration-none">
                                     <img src={EnhancedPreview} alt="portal preview" width="90%" />
                                     <h4 class="mt-3">Research Report Templates</h4>
                                 </Link>
-                                <p class="m-0 small">Web Development</p>
+                                <p className="m-0 small">Web Development</p>
                             </div>
                         </div>
-                        <div class="row my-5 py-5">
-                            <div class="col">
-                                <Link to="WebsiteRedesign" class="text-decoration-none">
-                                    <img src={RedesignPreview} alt="portal preview" width="90%" />
-                                    <h4 class="mt-3">BlueMatrix Website Redesign</h4>
-                                </Link>
-                                <p class="m-0 small">Web Design | Web Development</p>
-                            </div>
-                        </div>
-                        <div className="row my-5 py-5">
+                        <div className="row my-5 py-md-5 py-0">
                             <div className="col">
-                                <Link to="OfficialTemplate" class="text-decoration-none">
+                                <Link to="WebsiteRedesign" className="text-decoration-none">
+                                    <img src={RedesignPreview} alt="portal preview" width="90%" />
+                                    <h4 className="mt-3">BlueMatrix Website Redesign</h4>
+                                </Link>
+                                <p className="m-0 small">Web Design | Web Development</p>
+                            </div>
+                        </div>
+                        <div className="row my-5 py-md-5 py-0">
+                            <div className="col">
+                                <Link to="OfficialTemplate" className="text-decoration-none">
                                     <img src={ProductPreview} alt="Official Communication Template" width="90%" />
-                                    <h4 class="mt-3">Official Product Communications Template</h4>
+                                    <h4 className="mt-3">Official Product Communications Template</h4>
                                 </Link>
                                 <p class="m-0 small">Web Design | Web Development</p>
                             </div>
                         </div>
-                        <div class="row my-5 py-5">
-                            <div class="col">
-                                <a class="text-decoration-none" href="https://mjfloyd3.github.io/NYC-Knows/">
+                        <div className="row my-5 py-md-5 py-0">
+                            <div className="col">
+                                <a className="text-decoration-none" href="https://mjfloyd3.github.io/NYC-Knows/">
                                     <img src={NYCKnowsPreview} alt="NYC Knows" width="90%" />
-                                    <h4 class="mt-3">NYC Knows &#160; &#160;<i class="bi bi-box-arrow-up-right"></i></h4>
+                                    <h4 className="mt-3">NYC Knows &#160; &#160;<i class="bi bi-box-arrow-up-right"></i></h4>
                                 </a>
-                                <p class="m-0 small">Bootcamp Final Project | Web Development | Data Visualization</p>
+                                <p className="m-0 small">Bootcamp Final Project | Web Development | Data Visualization</p>
                             </div>
                         </div>
                 </div>
