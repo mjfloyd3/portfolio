1d4e1f0aa68eff0a015bf46c400b91ecd6817766
Jack <mjfloyd3@gmail.com>
2024-05-04 13:00:36 -0400

    day2point1

--- src/components/backtotop.js
+++ src/components/backtotop.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const scrollTop = () => {
     return ( 
-    <div className="d-flex justify-content-end p-5 me-5">
-        <a href="#root" className="btn btn-outline-secondary"><i class="bi bi-arrow-up-short"></i>  Back to Top</a>
+    <div className="d-flex justify-content-end p-md-5 p-0 me-5">
+        <a href="#/#root" className="btn btn-outline-secondary"><i class="bi bi-arrow-up-short"></i>  Back to Top</a>
     </div>
     )
 }
