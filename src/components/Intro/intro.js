c6aa607bf5371ca5132e637ea39144c0b15b21e5
Jack <mjfloyd3@gmail.com>
2024-05-08 09:56:30 -0400

    fontfamilychanges

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
                     <h1>Jack Floyd</h1>
                     <h2>UI Developer and Designer</h2>
                     <p class="lead fw-light mt-4">
-                        UI Developer with 7+ years experience developing and designing innovative web applications. I am interested in collaborating to develop meaningful solutions and build impactul, accessible user experiences; based in New York, NY.
+                        UI Developer with 7+ years experience developing and designing innovative web applications. I am interested in collaborating to develop meaningful solutions and build impactful, accessible user experiences; based in New York, NY.
                     </p>
                     </div>
                 </div>
